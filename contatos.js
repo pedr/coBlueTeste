d49db064e6c63e56888222c69af32ed0068857df
pedro l <pedrlz.frn@gmail.com>
2019-04-05 23:22:03 -0300

    rotas para update e delete

--- contatos.js
+++ contatos.js
@@ -16,6 +16,52 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await contatosService.getOne(id);
+    if (!result) {
+      res.sendStatus(404);
+      return;
+    }
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(400);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const result = await contatosService.update(data, id);
+    if (!result) {
+      res.sendStatus(404);
+      return;
+    }
+    res.status(201).json(result);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(400);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await contatosService.delete(id);
+    if (!result) {
+      res.sendStatus(404);
+      return;
+    }
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(400);
+  }
+});
+
 router.post('/', async (req, res) => {
   try {
     const data = req.body;
@@ -24,7 +70,7 @@ router.post('/', async (req, res) => {
       res.sendStatus(400);
       return;
     }
-    res.json(result);
+    res.status(201).json(result);
   } catch (err) {
     console.error(err);
     res.sendStatus(400);
--- contatosService.js
+++ contatosService.js
@@ -32,10 +32,65 @@ service.add = async (data) => {
   }
 };
 
+service.update = async (data, id) => {
+  try {
+    const {
+      nomeVendedor,
+      nomeEmpresa,
+      nomeContato,
+      email,
+      telefone,
+      dataContato,
+      dataValidade,
+    } = data;
+    const statement = 'UPDATE contato SET nome_vendedor= $1, nome_empresa = $2, nome_contato = $3, email = $4, telefone = $5, data_contato = $6, data_validade = $7 WHERE id = $8 RETURNING id, nome_vendedor AS "nomeVendedor", nome_empresa AS "nomeEmpresa", nome_contato AS "nomeContato", email, telefone, data_contato AS "dataContato", data_validade AS "dataValidade"';
+    const result = await database.customQuery(statement, [
+      nomeVendedor,
+      nomeEmpresa,
+      nomeContato,
+      email,
+      telefone,
+      dataContato,
+      dataValidade,
+      id,
+    ]);
+
+    return result;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+service.delete = async (id) => {
+  try {
+    const statement = 'DELETE FROM contato WHERE id = $1 RETURNING id';
+    const result = await database.customQuery(statement, [id]);
+
+    return result;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+
 service.getAll = async () => {
   try {
     const statement = 'SELECT id, nome_vendedor AS "nomeVendedor", nome_empresa AS "nomeEmpresa", nome_contato AS "nomeContato", email, telefone, data_contato AS "dataContato", data_validade AS "dataValidade" FROM contato ORDER BY data_contato DESC';
     const result = await database.customQuery(statement);
+
+    return result;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+service.getOne = async (id) => {
+  try {
+    const statement = 'SELECT id, nome_vendedor AS "nomeVendedor", nome_empresa AS "nomeEmpresa", nome_contato AS "nomeContato", email, telefone, data_contato AS "dataContato", data_validade AS "dataValidade" FROM contato WHERE id = $1';
+    const result = await database.customQuery(statement, [id]);
 
     return result;
   } catch (err) {
